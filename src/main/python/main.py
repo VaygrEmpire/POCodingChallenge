90debff070f15a2f99ff85f859d7741c94015327
Hyung Ro (Ryan) Yoon <ryanyoon.gamedev@gmail.com>
2023-03-14 10:11:59 -0400

    initial commit 3

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,18 +3,21 @@ import unittest
 class CodingChallenge:
 
     def main(self):
-        self.sumOfNumbers()
-
-    def sumOfNumbers(self, x, y):
+        a, b = None
 
         try:
-
-
+            a = input(int("Please enter first number"))
+            b = input(int("Please enter second number"))
 
         except:
             print("Please enter only numbers.")
 
-        result =
+        if (a != None or b != None):
+            self.sumOfNumbers(a, b)
+
+    # Create a function that takes two numbers as arguments and returns their sum.
+    def sumOfNumbers(self, x, y):
+        result = x+y
         print(result)
 
 class CodingChallengeTest(unittest.TestCase):
