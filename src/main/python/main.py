efe64954d006ac10adce6441bb6dfa6d5e0a24f8
Hyung Ro (Ryan) Yoon <ryanyoon.gamedev@gmail.com>
2023-03-20 16:48:38 -0400

    final commit

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,33 +5,89 @@ class CodingChallenge:
     def main(self):
 
         try:
-            a = int(input("Please enter first number: "))
-            b = int(input("Please enter second number: "))
+            a = 4
+            b = "4"
+            print(f"input a is {a}")
+            print(f'input b is "{b}"\n')
 
-            if (a != None or b != None):
-                print("Sum of Numbers are: ", self.sumOfNumbers(a, b))
+            if (a != None):
+                print(f"convert int to str is: '{self.int_to_str(a)}'")
+            if (b != None):
+                print(f"convert str to int is: {self.str_to_int(b)}\n")
+
+            self.printOutput(str(a), "s", 0)
+            self.printOutput(str(b), "s", 1)
+            self.printOutput(int(a), "i", 0)
+            self.printOutput(int(b), "i", 1)
+
         except:
-            print("Please enter only numbers.")
+            print("Try again")
+            return
 
+    # Python got drunk and the built-in functions str() and int() are acting odd:
+    # str(4) ➞ 4
+    # str("4") ➞ 4
+    # int("4") ➞ "4"
+    # int(4) ➞ "4"
+    #
+    # You need to create two functions to substitute str() and int().
+    # A function called int_to_str() that converts integers into strings and a function called str_to_int() that converts strings into integers.
 
+    # Examples:
+    # int_to_str(4) ➞ "4"
+    # str_to_int("4") ➞ 4
+    # int_to_str(29348) ➞ "29348"
 
-    # Create a function that takes two numbers as arguments and returns their sum.
-    def sumOfNumbers(self, x, y):
-        result = x+y
+    def int_to_str(self, x):
+        result = "{}".format(x)
+        print("type of int_to_str output is: ", type(result))
         return result
+
+    def str_to_int(self, x):
+        result = 0
+
+        for i in x:
+            result = result * 10 + (ord(i) - 48)
+
+        print("type of str_to_int output is: ", type(result))
+        return result
+
+    # de-drunk Python
+    def str(self, x):
+        return x.__str__()
+
+    def int(self, x):
+        return x.__int__()
+
+    def printOutput(self, function, s, i):
+        if (s == "s"):
+            if (i == 0):
+                print(f"de-drunk: output of str(a) is {function} and type of str(a) output is {type(function)}")
+            elif (i == 1):
+                print(f"de-drunk: output of str(b) is {function} and type of str(b) output is {type(function)}")
+        else:
+            if (i == 0):
+                print(f"de-drunk: output of int(a) is {function} and type of int(a) output is {type(function)}")
+            elif (i == 1):
+                print(f"de-drunk: output of int(b) is {function} and type of int(b) output is {type(function)}")
 
 class CodingChallengeTest(unittest.TestCase):
     CC = CodingChallenge()
 
     def testProb1(self):
-        sampleInput1 = 3
-        sampleInput2 = 2
+        sampleInput1 = 4
+        sampleInput2 = '4'
 
-        actualValue = self.CC.sumOfNumbers(sampleInput1, sampleInput2)
-        expectedValue = 5
+        actualValue1 = self.CC.int_to_str(sampleInput1)
+        expectedValue1 = '4'
 
-        self.assertEqual(actualValue, expectedValue)
-        print("test 1 completed: actual value is " + str(actualValue) + " and expected value is " + str(expectedValue))
+        actualValue2 = self.CC.str_to_int(sampleInput2)
+        expectedValue2 = 4
+
+        self.assertEqual(actualValue1, expectedValue1)
+        print("test 1 completed: actual value is " + str(actualValue1) + " and expected value is " + str(expectedValue1))
+        self.assertEqual(actualValue2, expectedValue2)
+        print("test 2 completed: actual value is " + str(actualValue2) + " and expected value is " + str(expectedValue2))
 
 if __name__ == '__main__':
     # unittest.main()
